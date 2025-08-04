eb174dafedc0a5269f61d9e0ffd6149dae7d345e
PTITVN <tle474498@gmail.com>
2025-08-05 00:04:44 +0700

    session2

--- session2/bt4.js
+++ session2/bt4.js
@@ -2,12 +2,11 @@ function checkElement (arr, value){
     if(!Array.isArray(arr)){
         return `Khong phai là mảng`
     }
-    arr.includes(value)? console.log(true)  : console.log(false);
-    
+   console.log(arr.includes(value));
 }
 let arr = [1,2,3,4,5];
 let value = 3;
-console.log(checkElement(arr,value));
+checkElement(arr,value);
 arr = [1,2,3,4,5];
 value = 6;
-console.log( checkElement(arr,value));
+checkElement(arr,value);
